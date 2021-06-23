fe1eac4feddcab5c136a2ef8161d955ce77b42b2
Stan-Leigh <64145417+Stan-Leigh@users.noreply.github.com>
2021-06-23 14:48:09 +0200

    Add files via upload

--- SQL practice 4.sql
+++ SQL practice 4.sql
@@ -33,3 +33,84 @@ from project
 left join student
 on student.student_id = project.student_id
 where student.student_id is null;
+
+create table details (
+	id integer,
+    name varchar(55)
+);
+
+insert into details (id, name)
+values (1, 'Obi Frank');
+
+insert into details (id, name)
+values (2, 'Tosin Emmanuel');
+
+truncate table details;
+
+insert into details (id, name)
+values (2, 'Obi Frank');
+
+insert into details (id, name)
+values (1, 'Tosin Emmanuel');
+
+delete from details;
+
+drop table details;
+
+create table profile (
+	id integer not null,
+    name varchar(55) not null,
+    age integer default 18
+);
+
+insert into profile (id, name)
+values (1, 'Obi Emeka');
+
+drop table profile;
+
+create table profile (
+	id integer,
+    name varchar(55) unique
+);
+
+insert into profile (id, name)
+values (1, 'Obi Emeka');
+
+insert into profile (id, name)
+values (2, 'Obi Emeka');
+
+create table stud (
+	student_id integer,
+    name varchar(55),
+    age integer,
+    primary key (student_id)
+);
+
+create table proj (
+	project_id integer,
+    title varchar (60),
+    student_id integer not null,
+    primary key(project_id),
+    foreign key(student_id) references stud (student_id)
+);
+
+alter table proj
+drop foreign key proj_ibfk_1;
+
+alter table proj
+add constraint Fk_student
+foreign key (student_id) references stud (student_id);
+
+drop table proj;
+drop table stud;
+
+create table stud(
+	student_id integer not null,
+    name varchar(55) not null,
+    age integer,
+    amount decimal(8, 2),
+    constraint ck_student check (age >= 18 and amount > 1000.00)
+);
+
+insert into stud (student_id, name, age, amount)
+values (1, 'Obi Frank', 18, 1100.00)
